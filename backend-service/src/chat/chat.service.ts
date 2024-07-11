7da1db7c8e0b5c0701b5323c920b7ca523585a04
viethoang <marcodev.haui@gmail.com>
2024-07-11 17:07:04 +0700

    feat: Lifecycle hook

--- backend-service/src/chat/chat.service.ts
+++ backend-service/src/chat/chat.service.ts
@@ -1,18 +1,34 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer
+  WebSocketServer,
+
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Constant } from '../constants/constant';
 
 @WebSocketGateway(Constant.SOCKET_PORT, { cors: true })
-export class ChatService {
+export class ChatService implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  afterInit(server: Server): any {
+    console.log('Websocket server initialized');
+  }
+
+  handleConnection(client: Socket, ...args): any {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket): any {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
   @WebSocketServer()
   server: Server;
-  
+
   @SubscribeMessage(Constant.SOCHIP_CHAT)
   handleMessage(
     @MessageBody() message: any,
